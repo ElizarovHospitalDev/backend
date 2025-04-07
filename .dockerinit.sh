ff74b4a3efbc72dcfcf3188c8d1646e9fcbab82d
velizhanin_ie <velizhanin_ie@its.dev>
2025-04-07 23:33:28 +0500

    run dev server

--- .dockerinit.sh
+++ .dockerinit.sh
@@ -2,4 +2,5 @@
 
 python /code/manage.py migrate --noinput;
 
-gunicorn -c gunicorn.conf.py elizarov.asgi:application --access-logfile -
+#gunicorn -c gunicorn.conf.py elizarov.asgi:application --access-logfile -
+python /code/manage.py runserver 0.0.0.0:8000
